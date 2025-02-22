915a781b148b465a5b7fb072a68b3d89d27da032
Carl <carl.hamann@live.de>
2025-02-22 09:09:27 +0100

    Wezterm: Formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,7 +15,7 @@ config.keys = {
 }
 --
 -- config.font = wezterm.font_with_fallback("Inconsolata Nerd Font")
-config.font = wezterm.font("Inconsolata Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font = wezterm.font("Inconsolata Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 -- https://github.com/prabirshrestha/dotfiles/blob/main/.config/wezterm/wezterm.lua
 
@@ -28,25 +28,19 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		args = { "pwsh.exe", "-NoLogo" },
 	})
 
-  -- Find installed visual studio version(s) and add their compilation
-  -- environment command prompts to the menu
-  for _, vsvers in
-    ipairs(
-      wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
-    )
-  do
-    local year = vsvers:gsub('Microsoft Visual Studio/', '')
-    table.insert(launch_menu, {
-      label = 'x64 Native Tools VS ' .. year,
-      args = {
-        'cmd.exe',
-        '/k',
-        'C:/Program Files (x86)/'
-          .. vsvers
-          .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
-      },
-    })
-  end
+	-- Find installed visual studio version(s) and add their compilation
+	-- environment command prompts to the menu
+	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+		local year = vsvers:gsub("Microsoft Visual Studio/", "")
+		table.insert(launch_menu, {
+			label = "x64 Native Tools VS " .. year,
+			args = {
+				"cmd.exe",
+				"/k",
+				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+			},
+		})
+	end
 end
 
 config.launch_menu = launch_menu
